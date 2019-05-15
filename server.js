a5088d5e84d5436713d86efa3613639b160a20ff
k8blanco <katiewhite303@gmail.com>
2019-05-14 21:02:23 -0600

    added in const passport

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-// const passport = require("passport");
+const passport = require("passport");
 const path = require("path");
 
 const app = express();
