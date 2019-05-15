48d8a35f9cefd4011e22e66f4f5e8fe78de02d43
Coloradosunrae <stevie@ourhopecenter.org>
2019-05-14 19:14:28 -0600

    change homepage

---
+++ client/build/precache-manifest.e72c5b691c87d06c1bca6f6c229afe17.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0de41b253dc139d05c0d5d6742a5292",
+    "url": "/static/media/welcomelogin.e0de41b2.gif"
+  },
+  {
+    "revision": "8efb1ea936d40031202ad4763d323e1b",
+    "url": "/static/media/phone.8efb1ea9.png"
+  },
+  {
+    "revision": "75bba1aa4832a0e53ad5608bd769753b",
+    "url": "/static/media/inperson.75bba1aa.png"
+  },
+  {
+    "revision": "481d286418ff85d45ebd87e9ab0218d3",
+    "url": "/static/media/form.481d2864.png"
+  },
+  {
+    "revision": "4b0a5fe60f73a74c8aaa5c7c4d4fcd0c",
+    "url": "/static/media/first1.4b0a5fe6.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "486831cd3535ffd935d2",
+    "url": "/static/js/main.6f840870.chunk.js"
+  },
+  {
+    "revision": "c23e4753f75ea26eeccc",
+    "url": "/static/js/2.3afea299.chunk.js"
+  },
+  {
+    "revision": "486831cd3535ffd935d2",
+    "url": "/static/css/main.65982bb4.chunk.css"
+  },
+  {
+    "revision": "af8b258d72613f1a4aec4d1969882540",
+    "url": "/index.html"
+  }
+];
